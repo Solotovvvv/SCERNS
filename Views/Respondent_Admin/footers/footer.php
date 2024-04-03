6afb6274ed7fee9929a26710644529bb49a649b3
dyiroooo <20200097m.ibia.jiroallen.bscs@gmail.com>
2024-04-03 17:56:22 +0800

    [Respondents] Respondent's Nav is now clearly defined.

---
+++ Views/Respondent_Admin/footers/footer.php
@@ -0,0 +1,12 @@
+<script src="https://adminlte.io/themes/v3/plugins/jquery/jquery.min.js">
+</script>
+<script src="https://adminlte.io/themes/v3/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script>
+    $.widget.bridge("uibutton", $.ui.button);
+</script>
+<script src="https://adminlte.io/themes/v3/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://adminlte.io/themes/v3/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="https://adminlte.io/themes/v3/dist/js/adminlte.js?v=3.2.0"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
