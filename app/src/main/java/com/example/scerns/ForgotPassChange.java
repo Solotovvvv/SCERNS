7134510ed3875f22bae6c79d1ac2f381a7614c32
Jiro <20200097m.ibia.jiroallen.bscs@gmail.com>
2024-04-12 18:58:24 +0800

    Forgot Password Steps Complete
    UIs of Forgot Password of Mobile Users are now readily available.

---
+++ app/src/main/java/com/example/scerns/ForgotPassChange.java
@@ -0,0 +1,12 @@
+package com.example.scerns;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotPassChange extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_pass_change_password);
+    }
+}
