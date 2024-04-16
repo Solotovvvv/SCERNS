fc0220547c2150b5ec4ef53ed2b5902e20d10497
Clarence Espejo <clarence.espejo013@gmail.com>
2024-04-16 17:09:01 +0800

    shared preference in login and main act and logout function and auto login

--- app/src/main/java/com/example/scerns/MainActivity.java
+++ app/src/main/java/com/example/scerns/MainActivity.java
@@ -1,21 +1,21 @@
 package com.example.scerns;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -32,9 +32,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userId = getIntent().getIntExtra("userId", -1);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        userId = sharedPreferences.getInt("userId", -1);
+
         if (userId != -1) {
-//            Toast.makeText(MainActivity.this, "User ID: " + userId, Toast.LENGTH_SHORT).show();
+             Toast.makeText(MainActivity.this, "User ID: " + userId, Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(MainActivity.this, "User ID not found", Toast.LENGTH_SHORT).show();
         }
@@ -76,24 +78,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             HomeFragment homeFragment = new HomeFragment();
             homeFragment.setUserId(userId);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
-//            Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_profile) {
             ProfileFragment profileFragment = new ProfileFragment();
             profileFragment.setUserId(userId);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profileFragment).commit();
-//            Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_contact) {
             EContactsFragment eContactsFragment = new EContactsFragment();
             eContactsFragment.setUserId(userId);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, eContactsFragment).commit();
-//            Toast.makeText(this, "Emergency Contacts clicked", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_history) {
             HistoryFragment historyFragment = new HistoryFragment();
             historyFragment.setUserId(userId); // Set the userId
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, historyFragment).commit();
-//            Toast.makeText(this, "History clicked", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_logout) {
-            Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show();
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.remove("userId");
+            editor.apply();
+            Intent loginIntent = new Intent(MainActivity.this, Login.class);
+            startActivity(loginIntent);
+            finish();
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
@@ -108,3 +112,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 }
+
